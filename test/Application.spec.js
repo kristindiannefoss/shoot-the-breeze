3728050eda9f27c77f023df5b9149ce77c5cc7ae
Andy Young <nxusmg@gmail.com>
2016-09-19 13:54:06 -0600

    Get a few emzyme tests to pass

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { shallow, mount, render } from 'enzyme';
 import { assert } from 'chai';
-
 import Application from '../lib/components/Application';
+import MessageList from '../lib/components/Application';
 
 describe('Application', () => {
 
@@ -11,5 +11,12 @@ describe('Application', () => {
     const wrapper = shallow(<Application />)
     assert.equal(wrapper.type(), 'div');
   });
+
+  it('allows us to set props in the messagelist', () => {
+    const wrapper = mount(<MessageList messages='test' />)
+    assert.equal(wrapper.props().messages, 'test')
+  })
+
+  it()
 
 });
