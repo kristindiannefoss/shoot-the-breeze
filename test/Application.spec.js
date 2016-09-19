928d4383e17be4e72665e949aa165016e02c09d3
Kris Foss <kris.foss@gmail.com>
2016-09-19 14:46:34 -0600

    adds import of components from application

--- test/Application.spec.js
+++ test/Application.spec.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
 import { shallow, mount, render } from 'enzyme';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import Application from '../lib/components/Application';
 import MessageList from '../lib/components/Application';
+import MessageInput from '../lib/components/Application';
 
 describe('Application', () => {
 
   it('renders as a <div>', () => {
-    const wrapper = shallow(<Application />)
+    const wrapper = shallow(<Application />);
     assert.equal(wrapper.type(), 'div');
   });
 
   it('allows us to set props in the messagelist', () => {
-    const wrapper = mount(<MessageList messages='test' />)
-    assert.equal(wrapper.props().messages, 'test')
-  })
+    const wrapper = mount(<MessageList messages='test' />);
+    assert.equal(wrapper.props().messages, 'test');
+  });
 
-  it()
+  it('finds action buttons', () => {
+    const wrapper = render(<MessageInput/>);
+    expect(wrapper.find('.ActionButton')).to.have.length(2);
+  });
 
 });
